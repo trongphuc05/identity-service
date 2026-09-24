b78876afb627d2ded6cc469992cc87e15bd4361b
TrongPhucX5 <lphuc2324@gmail.com>
2026-09-24 09:12:11 +0700

    integration test with testcontainer

--- src/test/java/com/trongphuc/identity_service/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/trongphuc/identity_service/controller/UserControllerIntegrationTest.java
@@ -14,15 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
 
@@ -30,22 +26,7 @@ import java.time.LocalDate;
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource("/test.properties")
-@Testcontainers
-public class UserControllerTest {
-    static final MySQLContainer MY_SQL_CONTAINER;
-
-    static {
-        MY_SQL_CONTAINER = new MySQLContainer("mysql:latest");
-        MY_SQL_CONTAINER.start();
-    }
-
-    @DynamicPropertySource
-    static void configureTestProperties(DynamicPropertyRegistry registry){
-        registry.add("spring.datasource.url",() -> MY_SQL_CONTAINER.getJdbcUrl());
-        registry.add("spring.datasource.username",() -> MY_SQL_CONTAINER.getUsername());
-        registry.add("spring.datasource.password",() -> MY_SQL_CONTAINER.getPassword());
-        registry.add("spring.jpa.hibernate.ddl-auto",() -> "create");
-    }
+public class UserControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -112,15 +93,15 @@ public class UserControllerTest {
         String content = objectMapper.writeValueAsString(request);
 
         // WHEN, THEN
-//        mockMvc.perform(MockMvcRequestBuilders
-//                        .post("/users")
-//                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                        .content(content))
-//                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-//                .andExpect(MockMvcResultMatchers.jsonPath("code")
-//                        .value(1003))
-//                .andExpect(MockMvcResultMatchers.jsonPath("message")
-//                        .value("Username must be at least 4 characters")
-//                );
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("code")
+                        .value(1003))
+                .andExpect(MockMvcResultMatchers.jsonPath("message")
+                        .value("Username must be at least 4 characters")
+                );
     }
 }
